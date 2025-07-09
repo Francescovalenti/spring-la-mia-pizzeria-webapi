8f737fa7ad60d9cb45a60237397638854367bfcd
francescovalenti <francescovalenti83@outlook.it>
2025-07-09 09:12:32 +0200

    feat:sistemazione cartelle

--- src/main/java/org/pizza/java/spring_la_mia_pizzeria_relazioni/SpringLaMiaPizzeriaWebapiApplication.java
+++ src/main/java/org/pizza/java/spring_la_mia_pizzeria_relazioni/SpringLaMiaPizzeriaWebapiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringLaMiaPizzeriaRelazioniApplication {
+public class SpringLaMiaPizzeriaWebapiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringLaMiaPizzeriaRelazioniApplication.class, args);
+		SpringApplication.run(SpringLaMiaPizzeriaWebapiApplication.class, args);
 	}
 
 }
